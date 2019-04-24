6e25da5e680d3009c217d4f88ae77bcb72f129e6
joanthanAguiar <49882998+joanthanAguiar@users.noreply.github.com>
2019-04-23 21:42:54 -0500

    Se deja enlazado desde el boton

--- psao4-13/realidad_aumentada.aspx.vb
+++ psao4-13/realidad_aumentada.aspx.vb
@@ -2,4 +2,7 @@
 Partial Class areasdeestudio
     Inherits System.Web.UI.Page
 
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("contenido_realidad_virtual.aspx")
+    End Sub
 End Class
