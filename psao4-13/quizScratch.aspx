62de6047b1e6a8fc667e9125b2c021aed4e1711f
danbalro <daniel_balanta@hotmail.com>
2019-05-09 15:38:39 -0500

    CambiosQuiz
    Commit to master

--- psao4-13/quizScratch.aspx
+++ psao4-13/quizScratch.aspx
@@ -22,6 +22,21 @@
             </div>
             <h1>Quiz Scratch</h1>
         </div>
+
+         ID ESTUDIANTE:&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" BackColor="#FFFFCC"></asp:TextBox>
+         NOMBRE:&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server" BackColor="#FFFFCC"></asp:TextBox>
+                <div id="mensaje">
+        
+                    <%
+                        Response.Write(valor)
+                        Response.Write(nota)
+                    %>
+            
+                 </div>
+
+
         <div class="flex-container">
         <!--Respuesta correcta radioButton1  -->
             <h2>1. El puntero del raton en scratch nos indica su posición en grados?</h2>
--- psao4-13/quizScratch.aspx.vb
+++ psao4-13/quizScratch.aspx.vb
@@ -1,9 +1,57 @@
 ﻿
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.ComponentModel
 Partial Class quizIngles
     Inherits System.Web.UI.Page
 
 
+    Public valor As String
+    'variable que guarda la nota
+    Public nota As String
+    'idarea 3
+    Public idArea As Integer
+    'nombre Scratch
+    Public nombreArea As String
+    'idestudiante
+    Public idEstudiante As Integer
+    'nombre estudiante
+    Public nombreEstudiante As String
+
     Protected Sub Bustton1_Click(sender As Object, e As EventArgs) Handles Bustton1.Click
+
+        idArea = 3
+
+        nombreArea = "Scratch"
+
+        idEstudiante = TextBox1.Text
+
+        nombreEstudiante = TextBox2.Text
+
+
+
+        If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False And RadioButton4.Checked = False And RadioButton5.Checked = False And RadioButton6.Checked = False And RadioButton7.Checked = False And RadioButton8.Checked = False And RadioButton9.Checked = False And RadioButton10.Checked = False And RadioButton11.Checked = False And RadioButton12.Checked = False And RadioButton13.Checked = False And RadioButton14.Checked = False And RadioButton15.Checked = False Then
+            valor = "Debe responder a todas las preguntas"
+            'MsgBox("debe responder todas las respuestas")
+            Return
+
+        End If
+        ' mensaje en la pagina web
+        valor = "Su resultado es: "
+        If RadioButton1.Checked Then
+            nota += 1
+        ElseIf RadioButton12.Checked Then
+            nota += 1
+        ElseIf RadioButton6.Checked Then
+            nota += 1
+        ElseIf RadioButton14.Checked Then
+            nota += 1
+        ElseIf RadioButton9.Checked Then
+            nota += 1
+        Else
+            nota = 0
+        End If
+
 
     End Sub
 End Class
