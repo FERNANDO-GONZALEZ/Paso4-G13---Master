045813a21d7d9c2fd4e1939f2290c348fc4a1434
Luis Fernando González Bermúdez <fcprimaveral@gmail.com>
2019-05-09 14:37:52 -0500

    comit de corrección de quizingles

--- psao4-13/quizIngles.aspx.vb
+++ psao4-13/quizIngles.aspx.vb
@@ -1,4 +1,6 @@
-﻿
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.ComponentModel
 Partial Class quizIngles
     Inherits System.Web.UI.Page
 
@@ -6,13 +8,26 @@ Partial Class quizIngles
     Public valor As String
     'variable que guarda la nota
     Public nota As String
-    'materia ingles0 que se envia a base de datos
-    Public materia As String
+    'idarea 1
+    Public idArea As Integer
+    'nombre ingles
+    Public nombreArea As String
+    'idestudiante
+    Public idEstudiante As Integer
+    'nombre estudiante
+    Public nombreEstudiante As String
 
     Protected Sub Bustton1_Click(sender As Object, e As EventArgs) Handles Bustton1.Click
 
+        idArea = 1
 
-        materia = "Ingles 0"
+        nombreArea = "Ingles 0"
+
+        idEstudiante = TextBox1.Text
+
+        nombreEstudiante = TextBox2.Text
+
+
 
         If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False And RadioButton4.Checked = False And RadioButton5.Checked = False And RadioButton6.Checked = False And RadioButton7.Checked = False And RadioButton8.Checked = False And RadioButton9.Checked = False And RadioButton10.Checked = False Then
             valor = "Debe responder a todas las preguntas"
@@ -32,9 +47,9 @@ Partial Class quizIngles
             nota += 1
         ElseIf RadioButton9.Checked Then
             nota += 1
-                        Else
-                            nota = 0
-                        End If
+        Else
+            nota = 0
+        End If
 
 
     End Sub
