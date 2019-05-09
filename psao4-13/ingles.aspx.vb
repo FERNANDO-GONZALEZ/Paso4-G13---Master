ae0f5a280bec1b4c8d40f4488644601958063513
Luis Fernando González Bermúdez <fcprimaveral@gmail.com>
2019-05-08 21:07:40 -0500

    INGLES CON CAMBIO EN EL DISEÑO

--- psao4-13/ingles.aspx.vb
+++ psao4-13/ingles.aspx.vb
@@ -1,15 +1,8 @@
-﻿
-Partial Class areasdeestudio
+﻿Partial Class areasdeestudio
     Inherits System.Web.UI.Page
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Response.Redirect("menu.aspx")
-    End Sub
-    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Response.Redirect("quizInglesI.aspx")
-    End Sub
-    Protected Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Response.Redirect("quizInglesII.aspx")
     End Sub
     Protected Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         Response.Redirect("contenido_ingles.aspx")
@@ -18,3 +11,4 @@ Partial Class areasdeestudio
         Response.Redirect("quizIngles.aspx")
     End Sub
 End Class
+
